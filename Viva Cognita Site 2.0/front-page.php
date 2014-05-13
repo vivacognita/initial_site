7d486dc3ca3be477b92a0dab56dc2b699e474e9a
tbranzov <branzov@vedena.net>
2014-05-13 15:17:23 +0300

    front page news v2

--- Viva Cognita Site 2.0/front-page.php
+++ Viva Cognita Site 2.0/front-page.php
@@ -29,8 +29,6 @@ get_header(); ?>
 					<div id="SecondRow">
 						<div class="flt-left home-col-1-2"></div>
 						<div class="flt-left home-col-2-2"><div id="RoadMap"><a href="<?php echo get_bloginfo('url'); ?>/?page_id=125" class="homebtn"></a></div></div>
-						<div class="flt-left home-col-3-2">
-						
 							<?php 
 								// The Query	
 								$thePage="cat=5";
@@ -39,10 +37,16 @@ get_header(); ?>
 										$the_query->the_post();
 										global $more; 
 										$more = 0;
+										$thumb = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'full' );
+										$thumb_url = $thumb['0'];
+										$news_permalink = get_permalink( $post->ID ); 
 							?>
+							<a href="<?php echo $news_permalink; ?>">
+						<div class="flt-left home-col-3-2">
+							<div id="NewsContainer"  style="background-image:url('<?php echo $thumb_url; ?>');">
+							<div id="NewsContent" class="news_2layer">
 							<h2><?php the_title();?></h2>
 							<?php /* the_date('d-m-Y', '<div class="front_page_news_date"> Дата:', '</div>'); */?>
-							<div class="front_page_news_thumb"><?php the_post_thumbnail('thumbnail'); ?> </div>	
 							<div class="front_page_news_body"><?php the_content('<div class="front_page_more_text">Виж цялата новина...</div>'); ?> </div>
 							<?php
 								} else {
@@ -50,7 +54,10 @@ get_header(); ?>
 								}
 								/* Restore original Post Data */
 								wp_reset_postdata();
-							?>	
+							?>
+							</div>
+							</div>
+							</a>
 						</div>				
 					</div>
 					
